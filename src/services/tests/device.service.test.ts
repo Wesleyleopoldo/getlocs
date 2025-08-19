afe004ddaaf6be507af2089132a707068a80f137
Não interessa quem é <you@example.com>
2025-08-19 19:04:09 -0300

    feature(Alterar nome do dispositivo): Testa função que altera nome do dispositivo
    Signed-off-by: Não interessa quem é <you@example.com>

--- src/services/tests/device.service.test.ts
+++ src/services/tests/device.service.test.ts
@@ -47,9 +47,9 @@ describe("createDevice()", () => {
 });
 
 describe("putName()", () => {
-    
+
     const user = { id: "123e4-abcde-4567", name: "Wesley Silva", email: "testewesley@gmail.com", password: "123456" };
-    
+
     const deviceData = {
         id: 1,
         name: 'Meu Samsung',
@@ -62,15 +62,60 @@ describe("putName()", () => {
         createdAt: new Date(),
     };
 
+    const newName = { name: "Nome alterado" }
+
     beforeEach(() => {
         jest.clearAllMocks();
     });
 
     it("Deve atualizar o nome do dispositivo", async () => {
         mockPrisma.user.findUnique.mockResolvedValueOnce(user);
-        mockPrisma.device.findUnique.mockResolvedValueOnce(user);
-        mockPrisma.device.update.mockResolvedValueOnce(user);
+        mockPrisma.device.findUnique.mockResolvedValueOnce(deviceData);
+        mockPrisma.device.update.mockResolvedValueOnce(newName);
 
+        const newDeviceName = await putDeviceName(user.id, deviceData.uuidUnique, newName.name);
+
+        console.log(newDeviceName);
+
+        expect(newDeviceName).toEqual({
+            name: newName.name,
+        });
+
+        expect(mockPrisma.device.update).toHaveBeenCalledWith({
+            where: { uuidUnique: deviceData.uuidUnique },
+            data: { name: newName.name },
+        });
+    });
+    
+    it("Deve retornar um erro de usuário não encontrado", async () => {
         
+        mockPrisma.user.findUnique.mockResolvedValueOnce(null);
+        mockPrisma.device.findUnique.mockResolvedValueOnce(deviceData);
+        mockPrisma.device.update.mockResolvedValueOnce(newName);
+        
+        await expect(putDeviceName(user.id, deviceData.uuidUnique, newName.name)).rejects.toMatchObject({
+            message: "Usuário não existe na base de dados...",
+            statusCode: 404,
+        });
+    });
+    
+    it("Deve retornar um erro de dispositivo não encontrado", async () => {
+    
+        mockPrisma.user.findUnique.mockReset(); // Resetar os mocks anteriores...
+        mockPrisma.device.findUnique.mockReset();
+        mockPrisma.device.update.mockReset();
+    
+        mockPrisma.user.findUnique.mockResolvedValueOnce(user);
+        mockPrisma.device.findUnique.mockResolvedValueOnce(null);
+        mockPrisma.device.update.mockResolvedValueOnce(newName);
+    
+        await expect(putDeviceName(user.id, deviceData.uuidUnique, newName.name)).rejects.toMatchObject({
+            message: "Dispositivo não existe na base de dados...",
+            statusCode: 404,
+        });
+    });
+
+    it("Deve retornar 403 Forbbiden se o usuário não for dono do dispositivo...", async () => {
+
     });
 });
